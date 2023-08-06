bd658adb870e88a4c04eb5f37de1e976f2d0b480
ali johar <johar.ali1@gmail.com>
2023-08-06 10:57:20 +0330

    Add sample html text

--- lib/screen/epub/epub_screen.dart
+++ lib/screen/epub/epub_screen.dart
@@ -4,13 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class EpubScreen extends StatelessWidget {
-  const EpubScreen({super.key});
+  const EpubScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    String htmlText = '<h1>Hello, this is some HTML text!</h1>'
-        '<p>Here is some more content.</p>'
-        '<p>You can add any valid HTML content here.</p>';
+    String htmlText = '''
+      <html>
+        <head>
+          <style>
+          body {
+          padding: 40px;
+          direction: rtl;
+          }
+            h1 {
+              font-size: 80px;
+            }
+            p {
+              color: grey;
+              font-size: 40px;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>این یک عنوان هست</h1>
+<p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.<br />إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.<br />ومن هنا وجب على المصمم أن يضع نصوصا مؤقتة على التصميم ليظهر للعميل الشكل كاملاً،دور مولد النص العربى أن يوفر على المصمم عناء البحث عن نص بديل لا علاقة له بالموضوع الذى يتحدث عنه التصميم فيظهر بشكل لا يليق.<br />هذا النص يمكن أن يتم تركيبه على أي تصميم دون مشكلة فلن يبدو وكأنه نص منسوخ، غير منظم، غير منسق، أو حتى غير مفهوم. لأنه مازال نصاً بديلاً ومؤقتاً.</p>
+<p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.<br />إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.<br />ومن هنا وجب على المصمم أن يضع نصوصا مؤقتة على التصميم ليظهر للعميل الشكل كاملاً،دور مولد النص العربى أن يوفر على المصمم عناء البحث عن نص بديل لا علاقة له بالموضوع الذى يتحدث عنه التصميم فيظهر بشكل لا يليق.<br />هذا النص يمكن أن يتم تركيبه على أي تصميم دون مشكلة فلن يبدو وكأنه نص منسوخ، غير منظم، غير منسق، أو حتى غير مفهوم. لأنه مازال نصاً بديلاً ومؤقتاً.</p>
+        </body>
+      </html>
+    ''';
 
     return Scaffold(
       appBar: AppBar(
@@ -57,22 +78,29 @@ class EpubScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Row(
+      body: Column(
         children: [
           Expanded(
-            child: WebView(
-              initialUrl: '',
-              onWebViewCreated: (WebViewController webViewController) {
-                // Load HTML content when the WebView is created
-                webViewController.loadUrl(Uri.dataFromString(
-                  htmlText,
-                  mimeType: 'text/html',
-                  encoding: Encoding.getByName('utf-8'),
-                ).toString());
-              },
+            child: Row(
+              children: [
+                Expanded(
+                  child: WebView(
+                    initialUrl: '',
+                    onWebViewCreated: (WebViewController webViewController) {
+                      // Load HTML content when the WebView is created
+                      webViewController.loadUrl(Uri.dataFromString(
+                        htmlText,
+                        mimeType: 'text/html',
+                        encoding: Encoding.getByName('utf-8'),
+                      ).toString());
+                    },
+                  ),
+                ),
+                const VerticalSeekBar(), // Add the vertical SeekBar here
+              ],
             ),
           ),
-          const VerticalSeekBar(), // Add the vertical SeekBar here
+          Container(padding: EdgeInsets.all(20), child: Text('12/344'))
         ],
       ),
     );
@@ -80,7 +108,7 @@ class EpubScreen extends StatelessWidget {
 }
 
 class VerticalSeekBar extends StatefulWidget {
-  const VerticalSeekBar({super.key});
+  const VerticalSeekBar({Key? key});
 
   @override
   _VerticalSeekBarState createState() => _VerticalSeekBarState();
