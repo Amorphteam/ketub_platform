f95cfc2cc12e6b6b9f26efd805425a84c6a34b8d
samane Tohidi <s.tohidi39@gmail.com>
2023-08-06 15:58:44 +0330

    Create Search view in LibraryScreen

--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -1,47 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:ketub_platform/models/book_item.dart';
+import 'package:ketub_platform/pages/search_page.dart';
+import 'package:ketub_platform/pages/toc_page.dart';
 import 'package:ketub_platform/util/temp_data.dart';
 
 class LibraryPage extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, // Number of columns
-          crossAxisSpacing: 10, // Spacing between columns
-          mainAxisSpacing: 10,
-          childAspectRatio: 0.9,// Spacing between rows
-        ),
-        itemCount: bookItems.length,
-        itemBuilder: (context, index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10), // Rounded corners
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/' + bookItems[index].imagePath),
-                      fit: BoxFit.contain,
-                    ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SearchPage(),
                   ),
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey),
+                ),
+
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    SizedBox(width: 10),
+                    Text('Search'),
+                  ],
                 ),
               ),
-              SizedBox(height: 5), // Add spacing between image and title
-              Text(
-                bookItems[index].title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 12,
-                ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // Number of columns
+                crossAxisSpacing: 10, // Spacing between columns
+                mainAxisSpacing: 10, // Spacing between rows
+                childAspectRatio: 0.9,
               ),
-            ],
-          );
-        },
+              itemCount: bookItems.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/' + bookItems[index].imagePath),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      bookItems[index].title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
