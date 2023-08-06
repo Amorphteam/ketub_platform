0475fff4728e5431bbdf0151821db0ece3085978
samane Tohidi <s.tohidi39@gmail.com>
2023-08-06 13:11:20 +0330

    Create nav page

---
+++ lib/pages/nav_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:ketub_platform/pages/library_page.dart';
+import 'package:ketub_platform/pages/toc_page.dart';
+
+class NavPage extends StatefulWidget {
+  @override
+  _NavPageState createState() => _NavPageState();
+}
+
+class _NavPageState extends State<NavPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    LibraryPage(),
+    TocPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.toc),
+            label: 'toc',
+          ),
+        ],
+      ),
+    );
+  }
+}
