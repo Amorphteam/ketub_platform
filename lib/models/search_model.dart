46c7c2901b7d8075f8e5f3e200f902a41be2cfb2
ali johar <johar.ali1@gmail.com>
2023-09-10 23:11:35 +0330

    Handle search in all books

---
+++ lib/models/search_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+class SearchModel {
+  final String? bookAddress;
+  final String? bookTitle;
+  final String? pageId;
+  final String? spanna;
+  final int searchCount;
+
+  SearchModel({
+    required this.bookAddress,
+    required this.bookTitle,
+    required this.pageId,
+    required this.spanna,
+    required this.searchCount,
+  });
+}
+
+
+class SearchHighlight {
+  final String searchHighlight;
+  final SearchIndex index;
+
+  SearchHighlight(this.searchHighlight, this.index);
+}
+
+class SearchIndex {
+   int startIndex;
+   int lastIndex;
+
+  SearchIndex(this.startIndex, this.lastIndex);
+}
+
+class MySearcher {
+  SearchIndex? searchInString(String? pageString, String sw, int i) {
+    // Implement your search logic here
+    // Replace this with actual search logic
+    return null;
+  }
+}
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,12 +1,108 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+import '../../models/search_model.dart';
+import '../../utils/search_helper.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final searchHelper = SearchHelper();
+  final bookNameSearchingController = StreamController<String>();
+  String searchWord = ''; // Store the user's search input
+  List<SearchModel> searchResults = [];
+
+  @override
+  void dispose() {
+    bookNameSearchingController.close();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  bool shouldStartSearch() {
+    return searchWord.length > 3;
+  }
+
+  void handleSearch() {
+    if (shouldStartSearch()) {
+      // Start the search only if the search word is longer than 3 letters
+      final stream = searchHelper.searchAllBooks(
+        allBooks, // Your list of book file paths
+        searchWord,
+        bookNameSearchingController,
+      );
+
+
+      setState(() {
+        searchResults = []; // Clear previous search results
+      });
+
+      stream.listen((List<SearchModel> results) {
+        setState(() {
+          searchResults = results; // Update the search results
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Book Search'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                searchWord = value;
+              });
+              handleSearch(); // Call handleSearch when the search word changes
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter search keyword',
+              contentPadding: EdgeInsets.all(16.0),
+            ),
+          ),
+          Expanded(
+            child: buildSearchResultsList(searchResults),
+          ),
+        ],
+      ),
     );
   }
+
+  Widget buildSearchResultsList(List<SearchModel> results) {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        final result = results[index];
+        return GestureDetector(
+          onTap: () {
+            print('result is ${result.pageId}');
+          },
+          child: ListTile(
+            title: Text(result.bookTitle!),
+            subtitle: Text(result.spanna!),
+            // You can customize the display of search results as needed
+          ),
+        );
+      },
+    );
+  }
+  // Define your list of book file paths
+  List<String> allBooks = [
+    '57.epub',
+    '58.epub',
+    '47.epub',
+    // Add more book file paths as needed
+  ];
 }
