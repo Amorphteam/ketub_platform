277b457ff250a7aa4c29acfdf5b124e4418d8ce8
ali johar <johar.ali1@gmail.com>
2023-09-19 16:43:29 +0330

    Clean css

--- lib/utils/style_handler.dart
+++ lib/utils/style_handler.dart
@@ -50,8 +50,21 @@ html {
 }
 
 body {
-    padding: 40px 20px !important;
+  text-indent: 0px !important;
+    padding: 40px 40px !important;
     overflow: !important;
+        word-break: break-word !important;
+    -webkit-hyphens: auto !important;
+    hyphens: auto !important;
+  direction: rtl;
+  text-align: justify;
+  text-decoration: none;
+  pointer-events: none;
+  cursor: default;
+}
+
+h1, h2 {
+    text-align: center !important;
 }
 
 /* Custom padding for tablets */
@@ -61,155 +74,12 @@ body {
     }
 }
 
-/* Table */
-table {
-    border-collapse: collapse;
-    border-spacing: 0;
-}
-tbody, tfoot, thead {
-    vertical-align: middle !important;
-}
-td, th, tr {
-    vertical-align: inherit !important;
-}
-
-/* List */
-dd, dir, menu, ol, ul { margin-left: 30px !important; }
-ol { list-style-type: decimal !important; }
-li { display: list-item !important; }
-ol ol, ol ul, ul ol, ul ul {
-    margin-bottom: 0 !important;
-    margin-top: 0 !important;
-}
-
-/* Links */
-a { -webkit-touch-callout: none; } /* Disable link callback */
-* { -webkit-user-select: text; }
-img { -webkit-user-select: none; }
-p {
-    text-indent: 1.25em;
-}
-b, strong, th {font-weight: bolder !important;}
-
-/* Forced font overrides */
-code, kbd, pre, samp, tt {
-    font-family: monospace, monospace !important;
-    font-size: 1em;
-}
-button, input, select, textarea { display: inline-block !important; }
-/*h1, h2, h3, h4, h5, h6 { font-weight: 400!important; }*/
-del, s, strike { text-decoration: line-through!important; }
-hr {
-    background-color: rgba(0,0,0,.1) !important;
-    border: none !important;
-    height: 1px !important;
-}
-
-
-/* Sub and Super */
-big { font-size: 1.15em !important; }
-small, sub, sup { font-size: .65em !important; }
-sub { vertical-align: sub !important; }
-sup {
-    font-family: monospace !important;
-    vertical-align: super !important;
-}
-
-
-/* iBooks like */
-a { text-decoration: none; }
-pre { white-space: pre-wrap; }
-@page { margin: 0 0 !important; }
-table, ol, il { text-align: -webkit-auto; }
-h1 ,h2 ,h3 ,h4 ,h5 ,h6 {
-    text-align: -webkit-auto;
-    text-rendering: optimizelegibility;
-}
-
-/* allow breaking of words on headers and anchors as they tend to be larger font size or contain longer words */
-a, h1, h2, h3, h4, h5, h6 {
-    word-break: break-word !important;
-    -webkit-hyphens: none !important;
-    hyphens: none !important;
-}
-
-/* Begin Ted */
-img, svg, audio, video {
-    max-height: 95% !important;
-    max-width: 100% !important;
-    box-sizing: border-box;
-    object-fit: contain;
-    page-break-inside: avoid;
-}
-
-/* End Ted */
-
-/* Divs are also used to size images so make sure the authors get what they intended */
-/* which is for the images boxed in them to be completely visible on screen */
-div { max-width: 100%; }
-aside[epub|type~="footnote"] { display: none !important; }
-ruby > rt, ruby > rp { -webkit-user-select: none; }
-* { -webkit-font-smoothing: subpixel-antialiased }
-
-
-/*
- *
- * Highlight classes
- *
- */
-
-highlight {
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-}
-
-/* Remove tap highlight */
-input, textarea, button, highlight, select, a {
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
-}
-
-/* Highlight styles */
-html .highlight-yellow {background:rgb(255, 235, 107)}
-html .highlight-green {background:#C0ED72}
-html .highlight-blue {background:#ADD8FF}
-html .highlight-pink {background:#FFB0CA}
-html .highlight-underline {
-    text-decoration: none;
-    border-bottom: 2px solid #F02814;
-}
-
-html .highlight-yellow, html .highlight-green, html .highlight-blue, html .highlight-pink, span.epub-media-overlay-playing {
-    border-radius: 3px;
-    padding: 0 2px;
-    margin: 0 -2px;
-}
-
-/* default media overlay style */
-.mediaOverlayStyle0 span.epub-media-overlay-playing {
-    background: #ccc
-}
-
-.mediaOverlayStyle1 .epub-media-overlay-playing {
-    border-bottom: dotted 2px transparent;
-    border-radius: 0;
-}
-
-
-
-/*
- *
- * Night mode
- *
- */
 
 html {
     -webkit-transition: all 0.6s ease;
-    background-color: #FFFFFF !important;
 }
 
-body {
-    background-color: transparent !important;
-}
+
 
 html.BLACK {
     background-color: #131313 !important;
@@ -260,15 +130,6 @@ html.DARK {
 }
 
 
-html.nightMode .highlight-yellow {background:rgba(255, 235, 107, 0.9)}
-html.nightMode .highlight-green {background:rgba(192, 237, 114, 0.9)}
-html.nightMode .highlight-blue {background:rgba(173, 216, 255, 0.9)}
-html.nightMode .highlight-pink {background:rgba(255, 176, 202, 0.9)}
-html.nightMode .highlight-underline {border-bottom: 2px solid rgba(240, 40, 20, 0.6)}
-
-/*
- * Mehdok custom bg color
- */
 
 html.CREAM {
     background-color: #f9efde !important;
@@ -297,19 +158,19 @@ html.LIGHT {
     font-family: 'font0', arial;
 }
 
-.FONT1, .FONT1 p, .FONT1 span, .FONT1 div {
+.FONT1, .FONT1 p, .FONT1 h1, .FONT1 h2, .FONT1 h3, .FONT1 h4, .FONT1 h5, .FONT1 h6, .FONT1 span, .FONT1 div {
 font-family: font1;
 }
 
-.FONT2, .FONT2 p, .FONT2 span, .FONT2 div {
+.FONT2, .FONT2 p, .FONT2 h1, .FONT2 h2, .FONT2 h3, .FONT2 h4, .FONT2 h5, .FONT2 h6 .FONT2 span, .FONT2 div {
 font-family: font2;
 }
 
-.FONT3, .FONT3 p, .FONT3 span, .FONT3 div {
+.FONT3, .FONT3 p, .FONT3 h1, .FONT3 h2, .FONT3 h3, .FONT3 h4, .FONT3 h5, .FONT3 h6, .FONT3 span, .FONT3 div {
 font-family: font3;
 }
 
-.FONT4, .FONT4 p, .FONT4 span, .FONT4 div {
+.FONT4, .FONT4 p, .FONT4 h1, .FONT4 h2, .FONT4 h3, .FONT4 h4, .FONT4 h5, .FONT4 h6, .FONT4 span, .FONT4 div {
 font-family: font4;
 }
 
@@ -373,45 +234,6 @@ html.SPACE2 { line-height: 2.2 !important; }
 html.SPACE3 { line-height: 2.5 !important; }
 html.SPACE4 { line-height: 2.7 !important; }
 
-h1 {
-    font-size: 1.5em;
-}
-h2 {
-    font-size: 1.2em;
-}
-h3 {
-    font-size: 1.0em;
-}
-h4 {
-    font-size: 0.8em;
-}
-h5 {
-    font-size: 0.75em;
-}
-h6 {
-    font-size: 0.67em;
-}
-body {
-    word-break: break-word !important;
-    -webkit-hyphens: auto !important;
-    hyphens: auto !important;
-}
-p, span, div {
-    font-size: 1em;
-}
-@media only screen and (min-device-width: 600px) {
-    div {
-        font-size: 1em;
-    }
-    body {
-        -webkit-hyphens: none !important;
-        hyphens: none !important;
-    }
-}
-
-.search_highlight {
-    color: red !important;
-}
 
 .book_name {
   text-align: center;
@@ -422,59 +244,16 @@ p, span, div {
 }
 
 
-body {
-    font-family: 'normal', arial;
-  direction: rtl;
-  text-align: justify;
-  text-decoration: none;
-  font-size: 40px;
-  pointer-events: none;
-  cursor: default;
-  }
 
-p{
- color: #6f6d6e;
-    direction: rtl;
-    text-align: justify;
-
-}
-
-h1{
-    font-family: 'heading1', arial;
-padding: 20px 0px 0px 0px !important;
-text-align: center !important;
-
-}
-
-h2,h3,h4,h5{
-    font-family: 'heading1', arial;
-padding: 20px 0px 0px 0px;
-
-
-}
 
 img{
-max-width: 500px;
+max-width: 1400px;
+min-width: 800px;
 display: block !important;
 margin-left: auto !important;
 margin-right: auto !important;
  }
 
-
-
-h6{
-background-color: #f4f4f4;
-text-align: center;
-}
-
-.Question{
-color: #6f6d6e;
-text-align: justify;
-
-}
-.pagenumbermanual{
-display: none;
-}
 
           .font1 {
               font-family: font1;
@@ -520,7 +299,7 @@ display: none;
           font-size: 30px !important;
           }
           
-          .LargeFontSize {
+          .largeFontSize {
           font-size: 50px !important;
           }
           
