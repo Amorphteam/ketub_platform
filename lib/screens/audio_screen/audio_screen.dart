5c441b351b0c3af5aa8b08ac13f17042a05b47bc
samane Tohidi <s.tohidi39@gmail.com>
2023-12-30 21:30:19 +0330

    temp

--- lib/screens/audio_screen/audio_screen.dart
+++ lib/screens/audio_screen/audio_screen.dart
@@ -1,7 +1,7 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'cubit/audio_cubit.dart';
 
 class AudioScreen extends StatefulWidget {
@@ -12,21 +12,53 @@ class AudioScreen extends StatefulWidget {
 }
 
 class _AudioScreenState extends State<AudioScreen> {
+  late AudioPlayer player;
+  bool isPlaying = false; // Added a variable to track audio playing state
+
   @override
   void initState() {
     super.initState();
+    player = AudioPlayer();
     _loadAudio();
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AudioCubit, AudioState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state is AudioLoadedState) {
+          // You can handle audio loaded state here if needed
+        }
+      },
       builder: (context, state) {
         if (state is AudioLoadedState) {
           final mediaDownloadLink = state.audioModel.mediaDownloadLink;
-          return Text('$mediaDownloadLink');
 
+          return Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Toggle the audio playback when the button is pressed
+                  if (isPlaying) {
+                    player.pause();
+                  } else {
+                    player.play(UrlSource(mediaDownloadLink!));
+                  }
+                  setState(() {
+                    isPlaying = !isPlaying; // Toggle the state
+                  });
+                },
+                child: Text(isPlaying ? 'Pause Audio' : 'Play Audio'),
+              ),
+              Text('$mediaDownloadLink'),
+            ],
+          );
         } else if (state is AudioErrorState) {
           return const Text('Error loading audio data');
         } else {
@@ -39,8 +71,9 @@ class _AudioScreenState extends State<AudioScreen> {
     );
   }
 
-  @override
   void _loadAudio() {
     BlocProvider.of<AudioCubit>(context).loadAudio(49581);
   }
 }
+
+
