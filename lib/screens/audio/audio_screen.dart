103677b7aafce84457f63f0237dd2ecece89dfc4
samane Tohidi <s.tohidi39@gmail.com>
2024-01-25 18:28:38 +0330

    temp 2

--- lib/screens/audio/audio_screen.dart
+++ lib/screens/audio/audio_screen.dart
@@ -1,4 +1,3 @@
-import 'package:audio_session/audio_session.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,14 +15,9 @@ class AudioScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) =>
-        AudioCubit()
-          ..loadAudio(49581),
-        child: AudioView(),
-      ),
+    return BlocProvider(
+      create: (context) => AudioCubit()..loadAudio(49581),
+      child: AudioView(),
     );
   }
 }
@@ -34,8 +28,6 @@ class AudioView extends StatefulWidget {
 }
 
 class _AudioViewState extends State<AudioView> {
-
-
   @override
   Widget build(BuildContext context) {
     return BlocListener<AudioCubit, AudioState>(
@@ -67,163 +59,180 @@ class _AudioViewState extends State<AudioView> {
 
   Widget _buildAudioPlayerUI() {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
-          child: StreamBuilder<SequenceState?>(
-            stream: AudioManager.player.sequenceStateStream,
-            builder: (context, snapshot) {
-              final state = snapshot.data;
-              if (state?.sequence.isEmpty ?? true) {
-                return const SizedBox();
-              }
-              final metadata = state!.currentSource!.tag as MediaItem;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                          child: Image.network(metadata.artUri.toString(),
-                            errorBuilder: (BuildContext context,
-                                Object exception, StackTrace? stackTrace) {
-                              return Image.asset(
-                                  'assets/images/audio_bk.jpg');
-                            },
-                          )
-
-                      ),
+        // Stack for Image, Centered Text, and Bottom Left Title
+        StreamBuilder<SequenceState?>(
+          stream: AudioManager.player.sequenceStateStream,
+          builder: (context, snapshot) {
+            final state = snapshot.data;
+            if (state?.sequence.isEmpty ?? true) {
+              return const SizedBox();
+            }
+            final metadata = state!.currentSource!.tag as MediaItem;
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                // Image at the top
+                Image.network(
+                  metadata.artUri.toString(),
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 200, // Adjust height as needed
+                  errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                    return Image.asset('assets/images/banner.png');
+                  },
+                ),
+                // Centered Text
+                Center(
+                  child: Text(
+                    metadata.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
                     ),
                   ),
-                  Text(metadata.album!, style: Theme
-                      .of(context)
-                      .textTheme
-                      .titleLarge),
-                  Text(metadata.title),
-                ],
-              );
-            },
-          ),
-        ),
-        AudioPlayerScreen(AudioManager.player),
-        StreamBuilder<PositionData>(
-          stream: AudioManager.positionDataStream,
-          builder: (context, snapshot) {
-            final positionData = snapshot.data;
-            return SeekBar(
-              duration: positionData?.duration ?? Duration.zero,
-              position: positionData?.position ?? Duration.zero,
-              bufferedPosition: positionData?.bufferedPosition ?? Duration.zero,
-              onChangeEnd: (newPosition) {
-                AudioManager.player.seek(newPosition);
-              },
+                ),
+                // Bottom Left Title
+                Positioned(
+                  left: 8.0,
+                  bottom: 8.0,
+                  child: Text(
+                    metadata.album!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
             );
           },
         ),
-        const SizedBox(height: 8.0),
-        Row(
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StreamBuilder<LoopMode>(
-              stream: AudioManager.player.loopModeStream,
+            AudioPlayerScreen(AudioManager.player),
+            StreamBuilder<PositionData>(
+              stream: AudioManager.positionDataStream,
               builder: (context, snapshot) {
-                final loopMode = snapshot.data ?? LoopMode.off;
-                const icons = [
-                  Icon(Icons.repeat, color: Colors.grey),
-                  Icon(Icons.repeat, color: Colors.orange),
-                  Icon(Icons.repeat_one, color: Colors.orange),
-                ];
-                const cycleModes = [
-                  LoopMode.off,
-                  LoopMode.all,
-                  LoopMode.one,
-                ];
-                final index = cycleModes.indexOf(loopMode);
-                return IconButton(
-                  icon: icons[index],
-                  onPressed: () {
-                    AudioManager.player.setLoopMode(cycleModes[
-                    (cycleModes.indexOf(loopMode) + 1) %
-                        cycleModes.length]);
+                final positionData = snapshot.data;
+                return SeekBar(
+                  duration: positionData?.duration ?? Duration.zero,
+                  position: positionData?.position ?? Duration.zero,
+                  bufferedPosition: positionData?.bufferedPosition ?? Duration.zero,
+                  onChangeEnd: (newPosition) {
+                    AudioManager.player.seek(newPosition);
                   },
                 );
               },
             ),
-            Expanded(
-              child: Text(
-                "Playlist",
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .titleLarge,
-                textAlign: TextAlign.center,
+            const SizedBox(height: 8.0),
+            Row(
+              children: [
+                StreamBuilder<LoopMode>(
+                  stream: AudioManager.player.loopModeStream,
+                  builder: (context, snapshot) {
+                    final loopMode = snapshot.data ?? LoopMode.off;
+                    const icons = [
+                      Icon(Icons.repeat, color: Colors.grey),
+                      Icon(Icons.repeat, color: Colors.orange),
+                      Icon(Icons.repeat_one, color: Colors.orange),
+                    ];
+                    const cycleModes = [
+                      LoopMode.off,
+                      LoopMode.all,
+                      LoopMode.one,
+                    ];
+                    final index = cycleModes.indexOf(loopMode);
+                    return IconButton(
+                      icon: icons[index],
+                      onPressed: () {
+                        AudioManager.player.setLoopMode(cycleModes[
+                        (cycleModes.indexOf(loopMode) + 1) %
+                            cycleModes.length]);
+                      },
+                    );
+                  },
+                ),
+                Expanded(
+                  child: Text(
+                    "Playlist",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                StreamBuilder<bool>(
+                  stream: AudioManager.player.shuffleModeEnabledStream,
+                  builder: (context, snapshot) {
+                    final shuffleModeEnabled = snapshot.data ?? false;
+                    return IconButton(
+                      icon: shuffleModeEnabled
+                          ? const Icon(Icons.shuffle, color: Colors.orange)
+                          : const Icon(Icons.shuffle, color: Colors.grey),
+                      onPressed: () async {
+                        final enable = !shuffleModeEnabled;
+                        if (enable) {
+                          await AudioManager.player.shuffle();
+                        }
+                        await AudioManager.player.setShuffleModeEnabled(enable);
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 240.0,
+              child: StreamBuilder<SequenceState?>(
+                stream: AudioManager.player.sequenceStateStream,
+                builder: (context, snapshot) {
+                  final state = snapshot.data;
+                  final sequence = state?.sequence ?? [];
+                  return ReorderableListView(
+                    onReorder: (int oldIndex, int newIndex) {
+                      if (oldIndex < newIndex) newIndex--;
+                      AudioManager.movePlaylistItem(oldIndex, newIndex);
+                    },
+                    children: [
+                      for (var i = 0; i < sequence.length; i++)
+                        Dismissible(
+                          key: ValueKey(sequence[i]),
+                          background: Container(
+                            color: Colors.redAccent,
+                            alignment: Alignment.centerRight,
+                            child: const Padding(
+                              padding: EdgeInsets.only(right: 8.0),
+                              child: Icon(Icons.delete, color: Colors.white),
+                            ),
+                          ),
+                          onDismissed: (dismissDirection) {
+                            AudioManager.removePlaylistItemAt(i);
+                          },
+                          child: Material(
+                            color: i == state!.currentIndex
+                                ? Colors.grey.shade300
+                                : null,
+                            child: ListTile(
+                              title: Text(sequence[i].tag.title as String),
+                              onTap: () {
+                                AudioManager.player.seek(Duration.zero, index: i);
+                              },
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                },
               ),
-            ),
-            StreamBuilder<bool>(
-              stream: AudioManager.player.shuffleModeEnabledStream,
-              builder: (context, snapshot) {
-                final shuffleModeEnabled = snapshot.data ?? false;
-                return IconButton(
-                  icon: shuffleModeEnabled
-                      ? const Icon(Icons.shuffle, color: Colors.orange)
-                      : const Icon(Icons.shuffle, color: Colors.grey),
-                  onPressed: () async {
-                    final enable = !shuffleModeEnabled;
-                    if (enable) {
-                      await AudioManager.player.shuffle();
-                    }
-                    await AudioManager.player.setShuffleModeEnabled(enable);
-                  },
-                );
-              },
             ),
           ],
         ),
-        SizedBox(
-          height: 240.0,
-          child: StreamBuilder<SequenceState?>(
-            stream: AudioManager.player.sequenceStateStream,
-            builder: (context, snapshot) {
-              final state = snapshot.data;
-              final sequence = state?.sequence ?? [];
-              return ReorderableListView(
-                onReorder: (int oldIndex, int newIndex) {
-                  if (oldIndex < newIndex) newIndex--;
-                  AudioManager.movePlaylistItem(oldIndex, newIndex);
-                },
-                children: [
-                  for (var i = 0; i < sequence.length; i++)
-                    Dismissible(
-                      key: ValueKey(sequence[i]),
-                      background: Container(
-                        color: Colors.redAccent,
-                        alignment: Alignment.centerRight,
-                        child: const Padding(
-                          padding: EdgeInsets.only(right: 8.0),
-                          child: Icon(Icons.delete, color: Colors.white),
-                        ),
-                      ),
-                      onDismissed: (dismissDirection) {
-                        AudioManager.removePlaylistItemAt(i);
-                      },
-                      child: Material(
-                        color: i == state!.currentIndex
-                            ? Colors.grey.shade300
-                            : null,
-                        child: ListTile(
-                          title: Text(sequence[i].tag.title as String),
-                          onTap: () {
-                            AudioManager.player.seek(Duration.zero, index: i);
-                          },
-                        ),
-                      ),
-                    ),
-                ],
-              );
-            },
-          ),
-        ),
+
       ],
     );
   }
