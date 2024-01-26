ed85e704e82de928389085dfbe4348c12a53f581
ali johar <johar.ali1@gmail.com>
2024-01-26 16:49:18 +0330

    Add splash page

--- lib/screens/main/main.dart
+++ lib/screens/main/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:ketub_platform/screens/main/nav_screen.dart';
+import 'package:ketub_platform/screens/splash/splash_screen.dart';
 import 'package:ketub_platform/utils/audio_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 Future<void> main() async {
@@ -31,13 +33,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'Flutter Demo',
+      title: 'Ketub Platform',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'tajwal',
         useMaterial3: true,
       ),
-      home:  NavScreen(),
+      home:  FutureBuilder(
+        future: isFirstLaunch(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else {
+            if (snapshot.data == true) {
+              return SplashScreen();
+            } else {
+              return NavScreen();
+            }
+          }
+        },
+      ),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
@@ -49,6 +64,15 @@ class MyApp extends StatelessWidget {
       ],
       locale: Locale('ar', ''),
     );
+  }
+
+  Future<bool> isFirstLaunch() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isFirstLaunch = prefs.getBool('first_launch') ?? true;
+    if (isFirstLaunch) {
+      await prefs.setBool('first_launch', false);
+    }
+    return isFirstLaunch;
   }
 }
 
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../main/nav_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          // Full screen background SVG
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/images/bk_large.svg', // Replace with your SVG file path
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Content at the bottom
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  // Add decoration if needed
+                  ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Image.asset(
+                    height: 100,
+                    width: 100,
+                    'assets/images/hob.png', // Replace with your image file path
+                    // Set size as needed
+                  ),
+                  SizedBox(height: 40),
+                  Center(
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      "التطبيق الرسمي لحيدر حب الله",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        'يتضمن هذا التطبيق أحدث وأكمل تعليقات وآراء وكتب ومقالات الشيخ حيدر حب الله.',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => NavScreen(),
+                        ),
+                      );                    },
+                    child: Text('إبدأ الآن'),
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
