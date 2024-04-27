aa2af57b737736b8fe054dfa5f92223b76faf91e
ali johar <johar.ali1@gmail.com>
2024-04-27 13:28:44 +0330

    Add MODIFY_AUDIO_SETTINGS permission in AndroidManifest.xml Import volume_watcher package in audio_screen.dart Implement volume checking logic in AudioScreenState Refactor ControlButtonsWidget to use StatefulWidget for handling play button clicks and functionality

--- lib/screens/audio/audio_screen.dart
+++ lib/screens/audio/audio_screen.dart
@@ -9,6 +9,7 @@ import 'package:ketub_platform/screens/audio/widgets/control_buttons_widget.dart
 import 'package:ketub_platform/screens/audio/widgets/waveform_seek_bar.dart';
 import 'package:ketub_platform/utils/data_helper.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:volume_watcher/volume_watcher.dart';
 
 import '../../utils/common.dart';
 import 'cubit/audio_player_cubit.dart';
@@ -146,7 +147,9 @@ class _AudioScreenState extends State<AudioScreen> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(height: 20,),
-        ControlButtonsWidget(player),
+        ControlButtonsWidget(player, onPlayClicked: (){
+          initPlatformState();
+        },),
           Container(
               height: 100.0,
               child: Center(
@@ -214,7 +217,23 @@ class _AudioScreenState extends State<AudioScreen> {
     );
   }
 
+
+  Future<void> initPlatformState() async {
+    double volume = await VolumeWatcher.getCurrentVolume;
+
+      if (volume <= 0.2) {
+        final snackBar = SnackBar(
+          content: Text('الصوت منخفض جداً'),
+          duration: Duration(seconds: 2),
+        );
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+  }
+
 }
+
+
 
 
 class AudioPlayerService {
